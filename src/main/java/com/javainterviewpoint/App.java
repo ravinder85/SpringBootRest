9ddb776711fc0af485e221d109e2c7e0910c3fe5
ravinder85 <srreddy2003@gmail.com>
2019-04-12 12:07:54 +0200

    MY FIRST COMMIT

--- src/main/java/com/javainterviewpoint/App.java
+++ src/main/java/com/javainterviewpoint/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        
+        //chANGED FOR TEST
         SpringApplication.run(App.class, args);
     }
 }
